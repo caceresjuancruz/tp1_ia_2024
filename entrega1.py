250f650a7b7f5c82790f9baa0aacad7016413b52
Juan Cruz Cáceres <106564947+caceresjuancruz@users.noreply.github.com>
2024-06-10 23:31:29 -0300

    update entrega1

--- entrega1.py
+++ entrega1.py
@@ -1,36 +1,41 @@
-from simpleai.search import (SearchProblem, astar)
+from itertools import combinations
+from simpleai.search import (SearchProblem, astar, greedy)
 from simpleai.search.viewers import (BaseViewer, WebViewer)
-
 
 #juego de llenado de frascos de colores
 #estado inicial
-INICIAL = [("verde", "azul", "rojo", "naranja"),     # frasco 1, notar el orden de los colores
-        ("azul", "rosa", "naranja"),              # frasco 2, notar que es de largo 3, queda un espacio vacío
-        ("rosa", "celeste", "verde", "verde"),    # frasco 3, notar cómo "verde" se repite 2 veces por los 2 cuartos iguales
-        ("rosa", "rojo", "celeste", "celeste"),   # frasco 4
-        ("rojo", "azul", "lila"),                 # frasco 5
-        ("verde", "naranja", "celeste", "rojo"),  # frasco 6
-        ("azul", "naranja", "rosa"),              # frasco 7
-        ("lila", "lila", "lila"),                 # frasco 8, notar la repetición de colores para cada cuarto
-        (),]                                       # frasco 9, notar que una tupla de largo 0 es un frasco vacío
+INICIAL = (("verde", "azul", "rojo", "naranja"),  # frasco 1
+           ("azul", "rosa", "naranja"),           # frasco 2
+           ("rosa", "celeste", "verde", "verde"), # frasco 3
+           ("rosa", "rojo", "celeste", "celeste"),# frasco 4
+           ("rojo", "azul", "lila"),              # frasco 5
+           ("verde", "naranja", "celeste", "rojo"),# frasco 6
+           ("azul", "naranja", "rosa"),           # frasco 7
+           ("lila", "lila", "lila"),              # frasco 8
+           ())                                    # frasco 9
+
+def cerrado(frasco):
+        return len(frasco) == 4 and len(set(frasco)) == 1
 
 class LlenadoDeFrascosProblem(SearchProblem):
     def actions(self, state):
         acciones_disponibles = []
+        frascos_combinados = combinations(state, 2)
 
-        for i in range(len(state)):
-            for j in range(len(state)):
-                if i != j and state[i] != () and len(state[j]) < 4 and (state[i][-1] == state[j][-1] or state[j] == ()):
-                    acciones_disponibles.append((i, j))
+        for origen, destino in frascos_combinados:
+            if state.index(origen) != state.index(destino) and origen and len(destino) < 4 and not cerrado(origen) and not cerrado(destino) and destino and (origen[-1] == destino[-1] or destino == ()):
+                acciones_disponibles.append((state.index(origen), state.index(destino)))
 
         return acciones_disponibles
 
     def result(self, state, action):
         state = list(state)
-        i, j = action
-        state[j] = state[j] + (state[i][-1],)
-        state[i] = state[i][:-1]
+        origen, destino = action
+        if state[origen]:
+            state[destino] = state[destino] + (state[origen][-1],)
+            state[origen] = state[origen][:-1]
         state = tuple(state)
+        print(state)
         return state
 
     def cost(self, state, action, state2):
@@ -53,13 +58,17 @@ class LlenadoDeFrascosProblem(SearchProblem):
 def jugar(frascos, dificil):
     problem = LlenadoDeFrascosProblem(INICIAL)
     if dificil:
-        result = astar(problem)
+        result = greedy(problem)
     else:
-        result = astar(problem, viewer=BaseViewer())
+        result = astar(problem) #viewer=BaseViewer()
     pasos = []
-    for action, state in result.path():
-        pasos.append(action)
+    if result is not None:
+        for action, state in result.path():
+            origen, destino = action
+            pasos.append((origen + 1, destino + 1))
+    else:
+        print("No se encontró solución.")
     return pasos
 
-#print(jugar(INICIAL, True))
+#jugar(INICIAL, False)
 
